b46e0f3b9f5ff5fca1d0f81322b31237c9dcd559
Oguguamakwa O.G. Obidike <164647505+OGobidike@users.noreply.github.com>
2024-12-23 15:45:26 -0500

    Update machine.py

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,47 @@
+from datetime import datetime
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestClassifier
+from joblib import dump, load
+
+
 class Machine:
 
     def __init__(self, df):
-        pass
+        self.name = 'Random Forest Classifier'
+
+        self.timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # year month day...
+
+        target_y = df['Rarity']
+        features_x = df.drop(columns=['Rarity'])
+
+        self.model = RandomForestClassifier(
+            n_estimators=100,
+            max_depth=30,
+            min_samples_split=5,
+            min_samples_leaf=2,
+            max_features='sqrt',
+            bootstrap=False,
+            random_state=42
+        )
+
+        self.model.fit(features_x, target_y)
+
 
     def __call__(self, feature_basis):
-        pass
+        prediction, *_ = self.model.predict(feature_basis)
+        probability, *_ = self.model.predict_proba(feature_basis)
+        return prediction, max(probability)
 
     def save(self, filepath):
-        pass
+        # make sure save method is dump()'ed into file name 'model.joblib()'
+        dump(self.model, "model.joblib")
+
 
     @staticmethod
     def open(filepath):
-        pass
+        model = load("model.joblib")
+
+        return model
 
     def info(self):
-        pass
+        return f'Currently running: {self.name}, at: {self.timestamp}'
